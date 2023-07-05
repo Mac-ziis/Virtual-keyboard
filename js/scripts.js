dbb11e78b111a0425cd3dfe6b4aeedb272b84a46
Mac-ziis <macg286@gmail.com>
2023-07-05 16:43:37 -0700

    Add functionality for the keys through javascript as well as the rest of the styline

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+const keyboard = {
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: [],
+  },
+
+  eventHandlers: {
+    oninput: null,
+    onclose: null,
+  },
+
+  properties: {
+    value: "",
+    capsLock: false,
+  },
+
+  init() {
+    // create main elements
+    this.elements.main = document.createElement("div");
+    this.elements.keysContainer = document.createElement("div");
+
+    // setup main elements
+    this.elements.main.classList.add("keyboard", "1keyboard--hidden");
+    this.elements.keysContainer.classList.add("keyboard-keys");
+
+    // add to DOM
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+  },
+
+  _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+
+      "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+
+      "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+
+      "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+
+      "space"
+
+    ];
+
+    // create HTML for an icon
+    const createIconHTML = (icon_name) => {
+      return `<i class="material-icons">${icon_name}</i>`;
+    };
+
+    keyLayout.forEach(key => {
+      const keyElement = document.createElement("button");
+      const innerLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+
+      // Add attributes and classes
+      keyElement.setAttribute("type", "button");
+      keyElement.classList.add("keyboard-key");
+
+      switch (key) {
+        case "backspace":
+          keyElement.classList.add("keyboard-key-wide");
+          keyElement.innerHTML = createIconHTML("backspace");
+      }
+    });
+  },
+
+  _triggerEvent(handlerName) {
+    console.log("Event Triggered! Event Name: + handlerName");
+  },
+
+  _toggleCapsLock() {
+    console.log("Caps Lock toggled!");
+  },
+
+  open(initialValue, oninput, onclose) {
+
+  },
+
+  close() {
+
+  },
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+  keyboard.init();
+});
